998d3c7936ac4c3ccacf62125603fb02c6ddc134
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-09 18:33:46 +0200

    Update objTask.js

--- objTask.js
+++ objTask.js
@@ -3,5 +3,11 @@ var scores = {};
 scores["Vadim Ishenko"] = 0;
 scores["Olexander Chertkov"] = 0;
 scores["Anastasia Daineko"] = 0;
+scores;
 
+scores["Vadim Ishenko"] += 2;
+scores;
+
+scores["Olexander Chertkov"] += 3;
+scores["Anastasia Daineko"] += 4;
 scores;
