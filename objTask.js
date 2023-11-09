92936387f02703c9220db2877e3e734f2b009756
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-09 21:47:19 +0200

    Update objTask.js

--- objTask.js
+++ objTask.js
@@ -1,15 +1,18 @@
 // #1: points counter
 var scores = {};
-scores["Vadim Ishenko"] = 0;
-scores["Olexander Chertkov"] = 0;
-scores["Anastasia Daineko"] = 0;
+scores["Vadim"] = 0;
+scores["Olexander"] = 0;
+scores["Anastasia"] = 0;
 scores;
 
-scores["Vadim Ishenko"] += 2;
+scores["Vadim"] += 2;
 scores;
 
-scores["Olexander Chertkov"] += 3;
-scores["Anastasia Daineko"] += 4;
+scores["Olexander"] += 3;
+scores["Anastasia"] += 4;
+scores;
+
+scores["Vadim"] -= 1;
 scores;
 
 // #2: we find out the essence of objects and arrays
